97ee3229629e4be9a112b5d252797004b1a60120
jacobrk0528 <jacobkrebs0528@yahoo.com>
2023-07-21 21:54:54 -0400

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -30,43 +30,61 @@ Route::get('/contact')
     ->name('contact')
     ->uses([ContactController::class, 'show']);
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/profile')
-        ->name('profile.show')
-        ->uses([UserController::class, 'show']);
+Route::get('/profile')
+    ->name('profile.show')
+    ->uses([UserController::class, 'show'])
+    ->middleware(['auth.session', 'verified']);
+    Route::get('/profile/success')
+    ->name('profile.success')
+    ->uses([UserController::class, 'showSuccess'])
+    ->middleware(['auth.session', 'verified']);
 
-    Route::get('/quiz')
-        ->name('quiz')
-        ->uses([QuizController::class, 'show']);
-});
+Route::get('/quiz')
+    ->name('quiz')
+    ->uses([QuizController::class, 'show'])
+    ->middleware(['auth.session', 'verified']);
 
-Route::get('/admin')->name('admin')->uses([AdminController::class, 'show'])->middleware('admin');
-Route::get('userProfile/{id}', [UserController::class, 'showOtherProfile'])->name('userProfile')->middleware('admin');
+Route::get('/admin')
+    ->name('admin')
+    ->uses([AdminController::class, 'show'])
+    ->middleware('admin');
+Route::get('userProfile/{id}', [UserController::class, 'showOtherProfile'])
+    ->name('userProfile')
+    ->middleware('admin');
 
-// resume
-Route::post('/file-upload', [resumeController::class, 'store'])->name('file.store');
-Route::get('userProfile/file-download/{userId}', [resumeController::class, 'downloadResume'])->name('file.download')->middleware('admin');
-Route::get('/file-uploaded/{userId}', [resumeController::class, 'isUploaded'])->name('resume-uploaded');
 
 // ------------------ API Routes ------------------ //
 
+// resume
+Route::post('/file-upload', [resumeController::class, 'store'])
+    ->name('file.store');
+Route::get('userProfile/file-download/{userId}', [resumeController::class, 'downloadResume'])
+    ->name('file.download')
+    ->middleware('admin');
+Route::get('/file-uploaded/{userId}', [resumeController::class, 'isUploaded'])
+    ->name('resume-uploaded');
+
 // Update User Profile
-Route::post('/updateUser', [UserController::class, 'update'])->name('updateUser');
+Route::post('/updateUser', [UserController::class, 'update'])
+    ->name('updateUser');
 
 // Send Contact Email
 Route::post('/send-email', [ContactController::class, 'sendEmail']);
 
 // Quiz routes
-Route::post('/setup-quiz', [QuizController::class, 'setup'])->name('setup-quiz');
-Route::get('/get-quiz-answer', [QuizController::class, 'getAnswer'])->name('get-quiz-answer');
-Route::post('/submit-quiz', [QuizController::class, 'submitQuiz'])->name('submit-quiz');
-Route::post('/update-quiz', [QuizController::class, 'update'])->name('submit-quiz');
-Route::get('/quiz-completed', [QuizController::class, 'isCompleted'])->name('quiz-completed');
+Route::post('/setup-quiz', [QuizController::class, 'setup'])
+    ->name('setup-quiz');
+Route::get('/get-quiz-answer', [QuizController::class, 'getAnswer'])
+    ->name('get-quiz-answer');
+Route::post('/submit-quiz', [QuizController::class, 'submitQuiz'])
+    ->name('submit-quiz');
+Route::post('/update-quiz', [QuizController::class, 'update'])
+    ->name('submit-quiz');
+Route::get('/quiz-completed', [QuizController::class, 'isCompleted'])
+    ->name('quiz-completed');
 
 // Admin routes
-Route::get('/sortQuizTableRows', [AdminController::class, 'sortRows'])->name('sortQuizTableRows');
-Route::get('/searchQuizTableRows', [AdminController::class, 'search'])->name('searchQuizTableRows');
+Route::get('/sortQuizTableRows', [AdminController::class, 'sortRows'])
+    ->name('sortQuizTableRows');
+Route::get('/searchQuizTableRows', [AdminController::class, 'search'])
+    ->name('searchQuizTableRows');
